79efe2bd9887d92c5f21306430d7774d71be1d19
Michał Okulewicz <m.okulewicz.phd@gmail.com>
2021-01-09 22:47:11 +0100

    refactored restart observer

--- gapso/src/main/java/pl/edu/pw/mini/gapso/optimizer/restart/MaxPopulationDistanceRestart.java
+++ gapso/src/main/java/pl/edu/pw/mini/gapso/optimizer/restart/MaxPopulationDistanceRestart.java
@@ -2,10 +2,8 @@ package pl.edu.pw.mini.gapso.optimizer.restart;
 
 import pl.edu.pw.mini.gapso.optimizer.Particle;
 
+import java.util.DoubleSummaryStatistics;
 import java.util.List;
-import java.util.OptionalDouble;
-import java.util.function.Supplier;
-import java.util.stream.DoubleStream;
 
 public class MaxPopulationDistanceRestart extends RestartObserver {
     private final double _threshold;
@@ -21,13 +19,16 @@ public class MaxPopulationDistanceRestart extends RestartObserver {
         int dim = particleList.get(0).getBest().getX().length;
         for (int i = 0; i < dim; ++i) {
             int finalI = i;
-            Supplier<DoubleStream> valuesInDimSupplier = () -> particleList.stream().mapToDouble(p -> p.getBest().getX()[finalI]);
-            OptionalDouble minOpt = valuesInDimSupplier.get().min();
-            OptionalDouble maxOpt = valuesInDimSupplier.get().max();
-            if (minOpt.isPresent() && maxOpt.isPresent()) {
-                if (maxOpt.getAsDouble() - minOpt.getAsDouble() < _threshold) {
-                    return true;
-                }
+            DoubleSummaryStatistics statistics =
+                    particleList
+                            .stream()
+                            .mapToDouble(p -> p.getBest()
+                                    .getX()[finalI])
+                            .summaryStatistics();
+            double minOpt = statistics.getMin();
+            double maxOpt = statistics.getMax();
+            if (maxOpt - minOpt < _threshold) {
+                return true;
             }
         }
         return false;
