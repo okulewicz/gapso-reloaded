51d6654863123a1d09ccfb0edd6fa2346fa4c3f6
Michał Okulewicz <m.okulewicz.phd@gmail.com>
2021-01-10 17:30:04 +0100

    Including restart manager and outer loop in optimization

--- gapso/src/main/java/pl/edu/pw/mini/gapso/optimizer/GAPSOOptimizer.java
+++ gapso/src/main/java/pl/edu/pw/mini/gapso/optimizer/GAPSOOptimizer.java
@@ -17,32 +17,47 @@ public class GAPSOOptimizer extends Optimizer {
     private final int _evaluationsBudget;
     private final Move[] _availableMoves;
     private final Initializer _initializer;
+    private final RestartObserver _restartObserver;
 
     public GAPSOOptimizer(int particlesCount, int evaluationsBudget, Move[] availableMoves, Initializer initializer, RestartObserver restartObserver) {
         _particlesCount = particlesCount;
         _evaluationsBudget = evaluationsBudget;
         _availableMoves = availableMoves;
         _initializer = initializer;
+        _restartObserver = restartObserver;
     }
 
     @Override
     public Sample optimize(Function function) {
-        List<Particle> particles = new ArrayList<>();
-        UpdatableSample globalBest = new UpdatableSample(
+        UpdatableSample totalGlobalBest = new UpdatableSample(
                 new SingleSample(
                         new double[function.getDimension()],
                         Double.POSITIVE_INFINITY)
         );
-        for (int i = 0; i < _particlesCount; ++i) {
-            double[] initialLocation = _initializer.getNextSample(function.getBounds());
-            particles.add(new Particle(initialLocation, function, globalBest));
-        }
         while (function.getEvaluationsCount() < _evaluationsBudget) {
-            for (Particle particle : particles) {
-                particle.move(_availableMoves[Generator.RANDOM.nextInt(_availableMoves.length)], particles);
+            UpdatableSample globalBest = new UpdatableSample(
+                    new SingleSample(
+                            new double[function.getDimension()],
+                            Double.POSITIVE_INFINITY)
+            );
+            List<Particle> particles = new ArrayList<>();
+            for (int i = 0; i < _particlesCount; ++i) {
+                double[] initialLocation = _initializer.getNextSample(function.getBounds());
+                particles.add(new Particle(initialLocation, function, globalBest));
+            }
+            while (function.getEvaluationsCount() < _evaluationsBudget) {
+                for (Particle particle : particles) {
+                    particle.move(_availableMoves[Generator.RANDOM.nextInt(_availableMoves.length)], particles);
+                }
+                if (_restartObserver.shouldBeRestarted(particles)) {
+                    break;
+                }
+            }
+            if (totalGlobalBest.getY() > globalBest.getY()) {
+                totalGlobalBest.updateSample(globalBest);
             }
         }
-        return globalBest;
+        return totalGlobalBest;
     }
 
     @Override
