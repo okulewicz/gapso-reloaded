c0f40ceb02ebfab76f5b784bc9fd13d68177f29c
Michał Okulewicz <m.okulewicz.phd@gmail.com>
2021-01-14 18:57:25 +0100

    Tested various options with spreads. I need to go back to tests to tune the spread and try to synchronize all details

--- gapso/src/main/java/pl/edu/pw/mini/gapso/initializer/BoundsManager.java
+++ gapso/src/main/java/pl/edu/pw/mini/gapso/initializer/BoundsManager.java
@@ -10,20 +10,22 @@ import pl.edu.pw.mini.gapso.optimizer.SamplingOptimizer;
 import pl.edu.pw.mini.gapso.sample.AllSamplesSampler;
 import pl.edu.pw.mini.gapso.sample.Sample;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
 
 public class BoundsManager {
-    public static final double SPREAD_FACTOR = 0.1;
+    public static final double INITIAL_SPREAD_FACTOR = 0.01;
     public static final int SAFETY_MULTIPLIER = 20;
     private Bounds initialBounds;
     private SplittableBounds bounds;
     private AllSamplesSampler sampler;
     private boolean modelUtilized;
+    private double spreadFactor;
 
     public void resetManager() {
         bounds = new SplittableBounds(initialBounds);
         sampler = new AllSamplesSampler();
+        spreadFactor = INITIAL_SPREAD_FACTOR;
     }
 
     public void registerObjectsWithOptimizer(SamplingOptimizer optimizer) {
@@ -39,8 +41,9 @@ public class BoundsManager {
     public Bounds getBounds() {
         assert initialBounds != null;
 
-        final int samplesForFullModel = 2 * SAFETY_MULTIPLIER * new FullSquareModel().getMinSamplesCount(initialBounds.getLower().length);
-        final int samplesForSimpleModel = 2 * SAFETY_MULTIPLIER * new SimpleSquareModel().getMinSamplesCount(initialBounds.getLower().length);
+        final int dimension = initialBounds.getLower().length;
+        final int samplesForFullModel = 2 * SAFETY_MULTIPLIER * new FullSquareModel().getMinSamplesCount(dimension);
+        final int samplesForSimpleModel = 2 * SAFETY_MULTIPLIER * new SimpleSquareModel().getMinSamplesCount(dimension);
         final boolean fullModelSatisfied = samplesForFullModel < sampler.getSamplesCount();
         final boolean simpleModelSatisfied = samplesForSimpleModel < sampler.getSamplesCount();
         Model functionModel;
@@ -52,21 +55,28 @@ public class BoundsManager {
             modelUtilized = false;
             return initialBounds;
         }
-        final int samplesForModeling = SAFETY_MULTIPLIER * functionModel.getMinSamplesCount(initialBounds.getLower().length);
+        final int samplesForModeling = SAFETY_MULTIPLIER * functionModel.getMinSamplesCount(dimension);
         if (sampler.getSamplesCount() > 2 * samplesForModeling) {
-            List<Sample> samplesForModel = sampler.getSamples(samplesForModeling);
-            double[] boundsCenter = functionModel.getOptimumLocation(samplesForModel, SimpleBounds.createBoundsFromSamples(samplesForModel));
-            double[] lower = Arrays.copyOf(boundsCenter, boundsCenter.length);
-            double[] upper = Arrays.copyOf(boundsCenter, boundsCenter.length);
+            List<double[]> estimations = new ArrayList<>();
             for (int j = 0; j < 10; ++j) {
-                samplesForModel = sampler.getSamples(samplesForModeling);
-                boundsCenter = functionModel.getOptimumLocation(samplesForModel, SimpleBounds.createBoundsFromSamples(samplesForModel));
-                if (functionModel.getrSquared() > 0.9) {
-                    for (int i = 0; i < boundsCenter.length; ++i) {
-                        lower[i] = Math.min(boundsCenter[i], lower[i]);
-                        upper[i] = Math.max(boundsCenter[i], upper[i]);
-                    }
+                List<Sample> samplesForModel = sampler.getSamples(samplesForModeling);
+                double[] boundsCenter = functionModel.getOptimumLocation(samplesForModel, SimpleBounds.createBoundsFromSamples(samplesForModel));
+                if (functionModel.getrSquared() > 0.95) {
+                    estimations.add(boundsCenter);
                 }
+            }
+            if (estimations.size() < 10) {
+                modelUtilized = false;
+                return initialBounds;
+            }
+            double[] lower = new double[dimension];
+            double[] upper = new double[dimension];
+            for (int i = 0; i < dimension; ++i) {
+                int finalI = i;
+                double mean = estimations.stream().mapToDouble(e -> e[finalI]).average().getAsDouble();
+                double sd = estimations.stream().mapToDouble(e -> Math.abs(e[finalI] - mean)).average().getAsDouble();
+                lower[i] = mean - 2 * sd;
+                upper[i] = mean + 2 * sd;
             }
 
             createSpreadForLowerAndUpperBounds(lower, upper);
@@ -84,10 +94,11 @@ public class BoundsManager {
 
     private void createSpreadForLowerAndUpperBounds(double[] lower, double[] upper) {
         for (int i = 0; i < lower.length; ++i) {
-            double spread = (initialBounds.getUpper()[i] - initialBounds.getLower()[i]) * SPREAD_FACTOR;
+            double spread = (initialBounds.getUpper()[i] - initialBounds.getLower()[i]) * spreadFactor;
             lower[i] = Math.max(initialBounds.getLower()[i], lower[i] - spread / 2.0);
             upper[i] = Math.min(initialBounds.getUpper()[i], upper[i] + spread / 2.0);
         }
+        spreadFactor *= 2;
     }
 
     public void registerOptimumLocation(Sample sample) {
