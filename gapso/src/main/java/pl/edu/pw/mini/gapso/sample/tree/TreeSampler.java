833cbc0daf885a37b337fc35f2125155cc25ac0a
Michał Okulewicz <m.okulewicz.phd@gmail.com>
2021-01-20 00:45:19 +0100

    limited capacity with switching index

--- gapso/src/main/java/pl/edu/pw/mini/gapso/sample/tree/TreeSampler.java
+++ gapso/src/main/java/pl/edu/pw/mini/gapso/sample/tree/TreeSampler.java
@@ -10,17 +10,24 @@ import java.util.stream.Collectors;
 public class TreeSampler extends Sampler {
     public static final int PAGE_SIZE = 20;
     private static final int MAX_CAPACITY = 3000;
-    private final RTree tree;
+    private RTree tree;
+    private RTree backupTree;
 
     public TreeSampler() {
         tree = new RTree(PAGE_SIZE);
+        backupTree = new RTree(PAGE_SIZE);
     }
 
     @Override
     public boolean tryStoreSample(Sample sample) {
         if (tree.getCount() > MAX_CAPACITY) {
             tree.clearIndex();
-            samplesCount = 0;
+            samplesCount = backupTree.getCount();
+            tree = backupTree;
+            backupTree = new RTree(PAGE_SIZE);
+        }
+        if (tree.getCount() > MAX_CAPACITY / 2) {
+            backupTree.indexSample(sample);
         }
         if (tree.indexSample(sample)) {
             samplesCount++;
