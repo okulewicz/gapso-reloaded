fb231123fb6795835cb53ce553a248ecb7054eea
Michał Okulewicz <m.okulewicz.phd@gmail.com>
2021-01-25 22:46:30 +0100

    Changed reset to SHADE style - 500D with no improvements

--- gapso/src/main/java/pl/edu/pw/mini/gapso/optimizer/restart/counter/NoImprovementRestartManager.java
+++ gapso/src/main/java/pl/edu/pw/mini/gapso/optimizer/restart/counter/NoImprovementRestartManager.java
@@ -2,25 +2,27 @@ package pl.edu.pw.mini.gapso.optimizer.restart.counter;
 
 import pl.edu.pw.mini.gapso.optimizer.Particle;
 import pl.edu.pw.mini.gapso.optimizer.restart.RestartManager;
+import pl.edu.pw.mini.gapso.sample.Sample;
 
 import java.util.List;
 
 public class NoImprovementRestartManager extends RestartManager {
     public static final String NAME = "NoImprovement";
-    final int MAX_TESTS = 10;
+    final int MAX_TESTS = 500;
     double bestValue = Double.POSITIVE_INFINITY;
     int tests = 0;
 
     @Override
     public boolean shouldBeRestarted(List<Particle> particleList) {
-        final double globalBestValue = particleList.get(particleList.get(0).getGlobalBestIndex()).getBest().getY();
+        final Sample best = particleList.get(particleList.get(0).getGlobalBestIndex()).getBest();
+        final double globalBestValue = best.getY();
         if (globalBestValue < bestValue) {
             bestValue = globalBestValue;
             tests = 0;
         } else {
             tests++;
         }
-        return MAX_TESTS < tests;
+        return MAX_TESTS * best.getX().length < tests;
     }
 
     @Override
